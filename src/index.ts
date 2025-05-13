a7fd8a290d52336e3cc86042d0c0483ee68647cf
tân nguyễn <tan0992727@gmail.com>
2025-05-13 12:50:01 +0700

    fix: change image port to 8000

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route";
 import noteRoutes from "./routes/note.route";
 
-
 const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }))
@@ -20,17 +19,14 @@ app.use(
 
 app.use(cookieParser());
 
-
-app.get("/", (req, res, next) => {
-    res.send("Hello World! 123")
+app.get("/", (req, res) => {
+    res.send("Hello World! 123");
 });
 
 app.use("/auth", authRoutes)
 app.use("/note", noteRoutes)
 
-//app.use(errorHandler);
-
-app.listen(PORT, async () => {
-    console.log("Server is running 123")
+app.listen(Number(PORT), "0.0.0.0", async () => {
+    console.log(`Server is running on port ${PORT}`);
     await connectToDatabase();
-})
+});
